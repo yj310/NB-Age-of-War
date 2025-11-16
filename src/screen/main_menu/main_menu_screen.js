e049a002c0152c77559e0ae06a9ef3f4e4b91257
Nia <yoenjii310@gmail.com>
2025-11-17 02:13:30 +0900

    draw_main_menu.js 제거

--- src/screen/main_menu/main_menu_screen.js
+++ src/screen/main_menu/main_menu_screen.js
@@ -2,7 +2,16 @@ class MainMenuScreen extends GameScreen {
     onEnter() { }
 
     render(screen) {
-        drawMainMenu(screen);
+        drawMainFrame();
+
+        translate(mainFrame.x, mainFrame.y);
+        textAlign(CENTER, CENTER);
+
+
+        this.drawTitleText();
+        this.drawPressAnyKey();
+
+        resetMatrix();
     }
 
     keyPressed(key) {
@@ -11,5 +20,31 @@ class MainMenuScreen extends GameScreen {
 
     mousePressed(mouseX, mouseY) {
         setGameState(GameState.playing);
+    }
+
+    drawPressAnyKey() {
+        textSize(18);
+
+        /// 텍스트 투명도 계산
+        const maxTransparency = 255;
+        const minTransparency = 50;
+        let value = (tick * 20) % ((maxTransparency - minTransparency) * 2);
+        let transparency = 255;
+        if (value <= (maxTransparency - minTransparency)) {
+            transparency = value + minTransparency;
+        } else {
+            value = value - (maxTransparency - minTransparency);
+            transparency = minTransparency + (maxTransparency - minTransparency) - value;
+        }
+
+        fill(`  #999999${transparency.toString(16)}`);
+        text("press any key to start", mainFrame.width / 2, 500);
+    }
+
+    drawTitleText() {
+        fill("#000000");
+        textSize(48);
+        text("인형 왕국 대작전!", mainFrame.width / 2, 150);
+
     }
 }
